aa5b4292ac42a19622f533a69b283f26133d96d9
MargaritaBusyginaCa <busygina.rita.ca@gmail.com>
2022-06-14 21:07:29 -0700

    add switching image functionality. When button is clicked its style should change, and the styles of other buttons should be deactivated

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,8 @@ import PlanetComponent from "./PlanetComponent";
 import Header from "./Header";
 
 function App(){
-    //setState for the array of planet objects
-    
-    
-    /* create displayPlanet function that will take planetName as a parameter
-       loop through planetArr and find a planet object that has the same name as planetName
-       when found, return the object
-    */
     return(
-        //render PlanetComponent here and pass displayPlanet function to it
+       
         <div>
           <Header/>  
         </div>
--- src/components/PlanetComponent.js
+++ src/components/PlanetComponent.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useState, useRef} from "react"
 import '../index.css'
 import '../styles/planet.css'
 import '../styles/factbox.css'
@@ -7,24 +7,76 @@ import {useParams} from "react-router-dom"
 import FactBox from "./FactBox"
 
 function PlanetComponent(){
-  // const [img, setImg] = (`../assets/planet-${thisPlanet.name.toLowerCase()}.svg`)  
   const {planetName} = useParams()
+  const imgRef1 = useRef()
+  const imgRef2 = useRef()
+  const imgRef3 = useRef()
+  const pRef1 = useRef()
+  const pRef2 = useRef()
+  const pRef3 = useRef()
+  const divRef1 = useRef()
+  const divRef2 = useRef()
+  const divRef3 = useRef()
   const thisPlanet = data.find(planet => planet.name === planetName)
-  function loadImage(imgName){
+  
+  // let mainImg = `../assets/planet-${thisPlanet.name.toLowerCase()}.svg`
+  // let inernalImg = `../assets/planet-${thisPlanet.name.toLowerCase()}-internal.svg`
+  // let geologyImg = `../assets/geology-${thisPlanet.name.toLowerCase()}.svg`
 
+  function loadImage(imgName){
+    if(imgName === "main"){
+      divRef1.current.className =  "interactive-btn active"
+      divRef2.current.className =  "interactive-btn"
+      divRef3.current.className =  "interactive-btn"
+      imgRef1.current.className = "main-img"
+      pRef1.current.className = "main-img"
+      imgRef2.current.className = "secondary-img"
+      pRef2.current.className = "secondary-img"
+      imgRef3.current.className = "secondary-img"
+      pRef3.current.className = "secondary-img"
+    }else if(imgName === "internal"){
+      divRef2.current.className =  "interactive-btn active"
+      divRef1.current.className =  "interactive-btn"
+      divRef3.current.className =  "interactive-btn"
+      imgRef2.current.className = "main-img"
+      pRef2.current.className = "main-img"
+      imgRef1.current.className = "secondary-img"
+      pRef1.current.className = "secondary-img"
+      imgRef3.current.className = "secondary-img"
+      pRef3.current.className = "secondary-img"
+    }else if(imgName ==="geology"){ 
+      divRef3.current.className =  "interactive-btn active"
+      divRef2.current.className =  "interactive-btn"
+      divRef1.current.className =  "interactive-btn"
+      imgRef3.current.className = "main-img"
+      pRef3.current.className = "main-img"
+      imgRef1.current.className = "main-img"
+      pRef1.current.className = "secondary-img"
+      imgRef2.current.className = "secondary-img"
+      pRef2.current.className = "secondary-img"
+
+    }
+ 
   }  
   return(
     <div className="planet-page">
       <div className="planet-container">
         <div className="planet-img">
-          <img src={require(`../assets/planet-${thisPlanet.name.toLowerCase()}.svg`).default}/>
+          <div className="main--geology-container">
+           <img src={require(`../assets/planet-${thisPlanet.name.toLowerCase()}.svg`).default} ref={imgRef1} className="main-img"/>
+           <img src={require(`../assets/geology-${thisPlanet.name.toLowerCase()}.png`).default} ref={imgRef3} className="secondary-img" id="geology-img"/>
+          </div>
+          <img src={require(`../assets/planet-${thisPlanet.name.toLowerCase()}-internal.svg`).default} ref={imgRef2} className="secondary-img"/>
+          
         </div>
 
         <div className="planet-info">
 
         <div className="planet-info-main">
           <h1>{thisPlanet.name.toUpperCase()}</h1>
-          <p>{thisPlanet.overview.content}</p>
+          <p ref={pRef1} className="main-img">{thisPlanet.overview.content}</p>
+          <p ref={pRef2} className="secondary-img">{thisPlanet.structure.content}</p>
+          <p ref={pRef3} className="secondary-img">{thisPlanet.geology.content}</p>
           <div className="source-info">
             <p>Source:</p>
             <a href={thisPlanet.overview.source}>Wikipedia</a>
@@ -33,15 +85,18 @@ function PlanetComponent(){
         </div>
 
         <div className="planet-buttons">
-          <div className="interactive-button-container" onClick={() => loadImage()}>
+          <div className="interactive-btn active" ref={divRef1}
+               onClick={() => loadImage("main")}>
             <p className="num">01</p>
             <p>overview</p>  
           </div>
-          <div className="interactive-button-container">
+          <div className="interactive-btn" ref={divRef2}
+               onClick={() => loadImage("internal")}>
             <p className="num">02</p>
             <p>internal structure</p>  
           </div>
-          <div className="interactive-button-container">
+          <div className="interactive-btn" ref={divRef3}
+               onClick={() => loadImage("geology")}>
             <p className="num">03</p>
             <p>surface geology</p>  
           </div>
